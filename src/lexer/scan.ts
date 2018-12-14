361d60a7825ab5d8b35651a1d72bcc4f948d5fbe
Kenny F <kfrachelle123@gmail.com>
2018-12-14 20:37:09 +0800

    fixed WS skipping
    This to show off how things are going to be now vs 1.6.8

--- src/lexer/scan.ts
+++ src/lexer/scan.ts
@@ -8,6 +8,30 @@ const unexpectedCharacter: (state: ParserState) => void = (state: ParserState) =
 
 const table = new Array(0xFFFF).fill(unexpectedCharacter, 0, 0x80)/*.fill(maybeIdentifier, 0x80)*/ as((state: ParserState, context: Context) => Token)[];
 
+table[Chars.Space] =
+table[Chars.Tab] =
+table[Chars.FormFeed] =
+table[Chars.VerticalTab] = () => {
+  return Token.WhiteSpace;
+}
+
+table[Chars.LineFeed] =
+table[Chars.Tab] =
+table[Chars.FormFeed] =
+table[Chars.VerticalTab] = state => {
+    state.flags |= Flags.LineTerminator;
+    return Token.WhiteSpace;
+}
+
+table[Chars.CarriageReturn] = state => {
+  state.flags |= Flags.LineTerminator;
+   // If it's a \r\n sequence, consume it as a single EOL.
+  if (state.index < state.length && state.currentChar === Chars.LineFeed) {
+      state.currentChar = state.source.charCodeAt(++state.index);
+  }
+  return Token.WhiteSpace;
+}
+
 /**
  * Scans and return the next token in the stream.,
  *
@@ -17,7 +41,7 @@ const table = new Array(0xFFFF).fill(unexpectedCharacter, 0, 0x80)/*.fill(maybeI
 export function nextToken(state: ParserState, context: Context): Token {
   while (state.index < state.length) {
       const currentChar = state.currentChar;
-      state.currentChar = state.source.charCodeAt(state.index++);
+      state.currentChar = state.source.charCodeAt(++state.index);
       if (((state.token = table[currentChar](state, context)) & Token.WhiteSpace) !== Token.WhiteSpace) {
         return state.token;
       }
