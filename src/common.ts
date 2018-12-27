4f1520dc46230f499238e4b1578bfde5244f59ab
kflash <kfrachelle123@gmail.com>
2018-12-27 14:45:29 +0800

    refactor(cherow): push the masks

--- src/common.ts
+++ src/common.ts
@@ -1,53 +1,93 @@
-import { Errors, report } from './errors';
-import * as ESTree from './estree';
-import { nextToken } from './lexer/scan';
-import { KeywordDescTable, Token } from './token';
-import { Location, ParserState } from './types';
 
 export const enum Context {
   Empty                  = 0,
-  OptionsRaw             = 1 << 1,
-  OptionsNext            = 1 << 2,
-  OptionsLoc             = 1 << 3,
-  OptionsRanges          = 1 << 4,
-  OptionsJSX             = 1 << 5,
-  OptionsWebCompat       = 1 << 6,
-  OptionsGlobalReturn    = 1 << 7,
-  OptionsCollectComments = 1 << 8,
-  OptionsExperimental    = 1 << 9,
-  OptionsNative          = 1 << 10,
-  OptionsTokenize        = 1 << 11,
-  ExpressionStart        = 1 << 12,
-  InGenerator            = 1 << 13,
-  InAsync                = 1 << 14,
-  InParam                = 1 << 15,
-  Strict                 = 1 << 16,
-  Module                 = 1 << 17,
-  TaggedTemplate         = 1 << 18,
-  Tokenize               = 1 << 19,
-  InClass                = 1 << 20,
-  NewTarget              = 1 << 21,
-  InFunctionBody         = 1 << 22,
-  DisallowIn             = 1 << 23,
-  RequireIdentifier      = 1 << 24,
-  DisallowGenerator      = 1 << 25,
-  InJSXChild             = 1 << 26,
-  //
-  Exponentation           = 1 << 27, // ECMAScript 7
-  TrailingComma           = 1 << 28, // ECMAScript 8
-  AsyncFunctions          = 1 << 29, // ECMAScript 8
-  AsyncGenerators         = 1 << 30, // ECMAScript 9
-  RevistedTemplate        = 1 << 31, // ECMAScript 9
-  ObjectSpread            = 1 << 32, // ECMAScript 9
+  OptionsRaw             = 1 << 0,
+  OptionsNext            = 1 << 1,
+  OptionsLoc             = 1 << 2,
+  OptionsRanges          = 1 << 3,
+  OptionsJSX             = 1 << 4,
+  OptionDisablesWebCompat  = 1 << 5,
+  OptionsGlobalReturn    = 1 << 6,
+  OptionsCollectComments = 1 << 7,
+  OptionsExperimental    = 1 << 8,
+  OptionsNative          = 1 << 9,
+  OptionsTokenize        = 1 << 10,
+  ExpressionStart        = 1 << 11,
+  InGenerator            = 1 << 12,
+  InAsync                = 1 << 13,
+  InArguments            = 1 << 14,
+  Strict                 = 1 << 15,
+  Module                 = 1 << 16,
+  TaggedTemplate         = 1 << 17,
+  SuperProperty          = 1 << 18,
+  InClass                = 1 << 19,
+  NewTarget              = 1 << 20,
+  InFunctionBody         = 1 << 21,
+  DisallowIn             = 1 << 22,
+  Global                 = 1 << 23,
+  DisallowGenerator      = 1 << 24,
+  ScopeRoot              = 1 << 25,
+  Statement              = 1 << 26,
+  InConstructor          = 1 << 27,
+  InMethod               = 1 << 28,
+  SuperCall              = 1 << 29
 
-  ES7            = Exponentation,
-  ES8            = Exponentation | TrailingComma | AsyncFunctions  | TrailingComma,
-  Next = Exponentation | TrailingComma | AsyncFunctions | AsyncGenerators | RevistedTemplate | ObjectSpread
 }
 
 export const enum Flags {
   Empty               = 0,
   LineTerminator      = 1 << 0,
-  ConsumedComment     = 1 << 1,
+  SeenPrototype        = 1 << 1,
 
 }
+
+// Expression origin
+export const enum ExpressionOrigin {
+  Expression = 1,
+  MemberExpression = 2,
+  Primary = 3,
+  Update = 4,
+  Ternary = 5,
+  Unary = 6,
+  Sequences = 7
+}
+
+// Binding type
+export const enum BindingType {
+  Empty     = 0,
+  Arguments = 1 << 0,
+  Variable  = 1 << 1,
+  Let       = 1 << 2,
+  Const     = 1 << 3,
+  Class     = 1 << 4
+}
+
+// Binding origin
+export const enum BindingOrigin {
+  Empty         = 0,
+  Statement     = 1 << 0,
+  For           = 1 << 1,
+  Export        = 1 << 2,
+  Catch         = 1 << 3,
+  AsyncArgs     = 1 << 4,
+  FunctionArgs  = 1 << 5,
+  Class         = 1 << 6,
+}
+
+// Scope flags
+export const enum ScopeFlags {
+  Empty         = 0,
+  ForStatement  = 1,
+  Block         = 2,
+  FunctionArgs  = 3,
+  CatchClause   = 4,
+  Switch        = 5
+}
+
+// Tracking eval and arguments
+export const enum EvalOrArguments {
+  Disallowed = 0,
+  Allowed = 1,
+}
+
+
