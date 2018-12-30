4a4faf589dab203559cbf7b52cca2512b2eda9eb
Kenny F <kfrachelle123@gmail.com>
2018-12-30 10:44:46 +0800

    refactor(cherow): updated demo with parser code

--- src/lexer_demo.ts
+++ src/lexer_demo.ts
@@ -909,3 +909,98 @@ export function scan(source: string) {
     };
   };
 }
+
+/** PARSER CODE **/
+
+import * as ESTree from './estree';
+import { scan } from './lexer';
+import { Options, EcmaVersion } from './types';
+import { Context } from './common';
+
+function parseSource(
+  source: string,
+  options: Options | void,
+  /*@internal*/
+  context: Context
+): any {
+  let sourceFile = '';
+
+  if (options !== undefined) {
+    // The option to specify ecamVersion
+    const ecmaVersion = options.ecmaVersion || 10;
+    options.ecmaVersion = <EcmaVersion>(ecmaVersion > 2009 ? ecmaVersion - 2009 : ecmaVersion);
+
+    // The flag to enable module syntax support
+    if (options.module) context |= Context.Module;
+    // The flag to enable stage 3 support (ESNext)
+    if (options.next) context |= Context.OptionsNext;
+    // The flag to enable React JSX parsing
+    if (options.jsx) context |= Context.OptionsJSX;
+    // The flag to enable start and end offsets to each node
+    if (options.ranges) context |= Context.OptionsRanges;
+    // The flag to enable line/column location information to each node
+    if (options.loc) context |= Context.OptionsLoc;
+    // The flag to attach raw property to each literal and identifier node
+    if (options.raw) context |= Context.OptionsRaw;
+    // The flag to allow return in the global scope
+    if (options.globalReturn) context |= Context.OptionsGlobalReturn;
+    // Set to true to record the source file in every node's loc object when the loc option is set.
+    if (!!options.source) sourceFile = options.source;
+    // The flag to enable implied strict mode
+    if (options.impliedStrict) context |= Context.Strict;
+    // The flag to enable experimental features
+    if (options.experimental) context |= Context.OptionsExperimental;
+    // The flag to enable "native" NodeJS / V8 features
+    if (options.native) context |= Context.OptionsNative;
+    // The flag to enable tokenizing
+    if (options.tokenize) context |= Context.OptionsTokenize;
+    // The flag to disable web compability (AnnexB)
+    if (options.disableWebCompat) context |= Context.OptionDisablesWebCompat;
+  }
+
+  const getToken = scan(source);
+
+  let currentToken = getToken(context);
+  let nextToken = getToken(context);
+
+  return {
+    value: 'fkleuver'
+  };
+}
+
+/**
+ * Parse either script code or module code
+ *
+ * @see [Link](https://tc39.github.io/ecma262/#sec-scripts)
+ * @see [Link](https://tc39.github.io/ecma262/#sec-modules)
+ *
+ * @param source source code to parse
+ * @param options parser options
+ */
+export function parse(source: string, options?: Options): ESTree.Program {
+  return options && options.module ? parseModule(source, options) : parseScript(source, options);
+}
+
+/**
+ * Parse script code
+ *
+ * @see [Link](https://tc39.github.io/ecma262/#sec-scripts)
+ *
+ * @param source source code to parse
+ * @param options parser options
+ */
+export function parseScript(source: string, options?: Options): ESTree.Program {
+  return parseSource(source, options, Context.None);
+}
+
+/**
+ * Parse module code
+ *
+ * @see [Link](https://tc39.github.io/ecma262/#sec-modules)
+ *
+ * @param source source code to parse
+ * @param options parser options
+ */
+export function parseModule(source: string, options?: Options): ESTree.Program {
+  return parseSource(source, options, Context.Strict | Context.Module);
+}
